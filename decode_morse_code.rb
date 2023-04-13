b95d83e37ab2b399fda601fe195f1f31e9a6c140
SFarida <faridagueleu@gmail.com>
2023-04-13 15:09:01 +0100

    Add decode message method

--- decode_morse_code.rb
+++ decode_morse_code.rb
@@ -8,3 +8,23 @@ def decode_char(char)
 
   morse_map[char]
 end
+
+def decode_word(string)
+  word = ''
+  array = string.split
+  array.each do |elem|
+    word += decode_char(elem)
+  end
+  word
+end
+
+def decode_message(string)
+  secret_message = ''
+  message_array = string.split('   ')
+  message_array.each do |word|
+    secret_message += "#{decode_word(word)} "
+  end
+  puts secret_message
+end
+
+decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
